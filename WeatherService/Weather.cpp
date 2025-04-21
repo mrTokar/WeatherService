4632ca64d9f9f4fff7ca6661dcacdd3d1b947b24
Egor Tokarev <egortok2014@gmail.com>
2025-04-21 10:29:53 +0300

    Add Weather.print_weather()

--- WeatherService/Weather.cpp
+++ WeatherService/Weather.cpp
@@ -1,4 +1,5 @@
 #include "Weather.h"
+#include <iostream>
 
 Weather::Weather(string const& city, double lon, double lat, double temperature, string weather, double windSpeed, int clouds):
 	city(city), weather(weather)
@@ -8,4 +9,14 @@ Weather::Weather(string const& city, double lon, double lat, double temperature,
 	this->temperature = temperature;
 	this->windSpeed = windSpeed;
 	this->clouds = clouds;
+}
+
+void Weather::print_weather() const
+{
+	cout << "Сегодня в " << city;
+	cout << "(" << lon << "°, " << lat << "°) ";
+	cout << weather;
+	cout << ". Температура на улице " << temperature << "°C, ";
+	cout << "а тажке небольшой ветер - всего " << windSpeed << "м/с. ";
+	cout << "На небе " << clouds << " облаков." << endl;
 }
